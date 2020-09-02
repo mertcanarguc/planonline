829a9f7e2ba610579eb0963163f67c68b0be6eea
Mertcan Arguc <arguc.mertcan@gmail.com>
2020-09-02 12:36:26 +0300

    jkabsdkjlasd

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const passport = require('passport');
+const User = require('../models/user');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/register', function(req, res) {
+    res.render('front/register', { });
 });
+
+router.post('/register', function(req, res) {
+    User.register(new User({ username: req.body.username}),
+    req.body.password, function(err, user) {
+   	 if (err) {
+   		 return res.render('front/register', { user: user });
+   	 }
+   	 
+   	 passport.authenticate('local')(req,res, function() {
+   		 res.redirect('/');
+   	 });
+    });
+});
+
+router.get('/login',function(req, res) {
+    res.render('front/login', { user : req.user });
+});
+
+router.post('/login', passport.authenticate('local'), function(req,res) {
+    res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
 
 module.exports = router;
